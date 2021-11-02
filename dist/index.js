b80e5a91846494e5a4e18ae51e73feb9352df381
Mikhail <mihail.kuznetsov.99@mail.ru>
2021-11-02 15:43:08 +0300

    done

--- dist/index.js
+++ dist/index.js
@@ -134,20 +134,6 @@ dropdownBtn.addEventListener('click', () => {
     }
 })
 
-jQuery(document).ready(function ($) {
-    $("a.nav").on("click", function (e) {
-        e.preventDefault();
-        var anchor = $(this).attr('href');
-        $('html, body').stop().animate({
-            scrollTop: $(anchor).offset().top - 60
-        }, 1000);
-    });
-    function slide() {
-        $(".embla__button--next").click();
-    }
-    setInterval(slide, 3000);
-});
-
 smoothLinks.forEach((element) => {
     element.addEventListener("click", () => {
         document.querySelector(".input").checked = false;
@@ -192,26 +178,48 @@ embla.on("init", disablePrevAndNextBtns);
 
 if (window.outerWidth < 768) {
     document.querySelectorAll(".mobile-seach").forEach((element) => {
-        element.removeAttribute('href')
+        element.removeAttribute('href');
         element.addEventListener('click', () => {
-            document.querySelector(".select").style.display = "flex"
+            document.querySelector(".select").classList.add("activeNav");
             overlay.style.display = "block";
             body.style.overflow = 'hidden';
-
+    
             if (element.id === 'anino') {
                 document.querySelector(".yandex").setAttribute('href', 'https://yandex.ru/maps/213/moscow/house/ulitsa_kirpichnyye_vyyemki_14s4/Z04YcAZhSUcDQFtvfXl5cnRlYg==/?ll=37.611024%2C55.583866&z=16.87')
                 document.querySelector(".google").setAttribute('href', 'https://www.google.ru/maps/place/Кирпичные+Выемки+ул.,+14+строение+4,+Москва,+117535/@55.583847,37.6089293,17z/data=!3m1!4b1!4m5!3m4!1s0x414aadf4cb581e5b:0x6d1d7acbc587aa99!8m2!3d55.583844!4d37.611118?hl=ru')
                 document.querySelector(".navi").setAttribute('href', 'yandexnavi://build_route_on_map?lat_to=55.583866&lon_to=37.611024')
+            }else if (element.id === 'reutov') {
+                document.querySelector(".yandex").setAttribute('href', 'https://yandex.ru/maps/21621/reutov/house/prospekt_mira_36a/Z04YfgFoTEUPQFtvfXt2cXllbA==/?ll=37.868507%2C55.770568&z=16.66')
+                document.querySelector(".google").setAttribute('href', 'https://www.google.ru/maps/place/пр.+Мира,+36а,+Реутов,+Московская+обл.,+143960/@55.7727133,37.8645669,17z/data=!3m1!4b1!4m5!3m4!1s0x414acb08556b9b1d:0x40ff9bbb71b49463!8m2!3d55.7727103!4d37.8667556')
+                document.querySelector(".navi").setAttribute('href', 'yandexnavi://build_route_on_map?lat_to=55.770568&lon_to=37.868508')
+            }else {
+                document.querySelector(".yandex").setAttribute('href', 'https://yandex.ru/maps/213/moscow/house/avtozavodskaya_ulitsa_23s436/Z04YcARkSEIEQFtvfXp5eH9jbQ==/?ll=37.634173%2C55.689309&z=16.66')
+                document.querySelector(".google").setAttribute('href', 'https://www.google.ru/maps/place/Автозаводская+ул.,+23,+Москва,+115280/@55.7035373,37.6320998,17z/data=!3m1!4b1!4m5!3m4!1s0x46b54b48d6ca2f37:0x656a608db27a9e27!8m2!3d55.7035343!4d37.6342885')
+                document.querySelector(".navi").setAttribute('href', 'yandexnavi://build_route_on_map?lat_to=55.689309&lon_to=37.634173')
             }
         })
     })
     
     document.getElementById('close').addEventListener("click", () => {
-        document.querySelector(".select").style.display = "none"
+        document.querySelector(".select").classList.remove("activeNav");
         overlay.style.display = "none";
         body.style.overflow = 'scroll';
     })
 }
 
+
+jQuery(document).ready(function ($) {
+    $("a.nav").on("click", function (e) {
+        e.preventDefault();
+        var anchor = $(this).attr('href');
+        $('html, body').stop().animate({
+            scrollTop: $(anchor).offset().top - 60
+        }, 1000);
+    });
+    function slide() {
+        $(".embla__button--next").click();
+    }
+    setInterval(slide, 3000);
+});
 
 
